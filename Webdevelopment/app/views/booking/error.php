6250d9f31111322a11a71ecab50647dd4557d153
Sander2501 <smmeijer2501@hotmail.nl>
2025-12-14 21:07:15 +0100

    add view

---
+++ Webdevelopment/app/views/booking/error.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html lang="en"><meta charset="utf-8"><title>Booking Failed</title>
+<h2 style="color:#b00">Booking failed</h2>
+<p><?= htmlspecialchars($error ?? 'Unknown error') ?></p>
+<p><a href="/classes">Back</a></p>
+</html>
