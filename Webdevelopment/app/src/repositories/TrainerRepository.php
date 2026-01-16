d19e916f643d2ff54fd73900da57b13d9de0802a
Sander2501 <smmeijer2501@hotmail.nl>
2026-01-16 19:41:56 +0100

    update

--- Webdevelopment/app/src/repositories/TrainerRepository.php
+++ Webdevelopment/app/src/repositories/TrainerRepository.php
@@ -1,5 +1,4 @@
 <?php
-//
 
 namespace App\Repositories;
 
@@ -14,17 +13,16 @@ class TrainerRepository extends BaseRepository implements ITrainerRepository
         $stmt = $this->pdo->query($sql);
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
-    
+
     public function findById(int $id): ?array
     {
-        // FIXED: Removed the space between ':' and 'id'
         $sql = "SELECT * FROM trainers WHERE id = :id LIMIT 1";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(['id' => $id]);
         $result = $stmt->fetch(\PDO::FETCH_ASSOC);
         return $result ?: null;
     }
-    
+
     public function getBlogPostsByTrainer(int $trainerId): array
     {
         $sql = "SELECT * FROM blog_posts WHERE trainer_id = :trainer_id ORDER BY published_at DESC";
